1e0afc43815e29911363f3de8729c953fb9f8536
Robin Appelman <robin@icewind.nl>
2024-04-05 13:47:33 +0200

    export patch option is now upstream

--- extractor.nix
+++ extractor.nix
@@ -1,18 +1,13 @@
 {
   lib,
   writeShellApplication,
-  patch,
   jq,
 }:
 writeShellApplication {
   name = "nvidia-patch-extrator";
-  runtimeInputs = [patch jq];
+  runtimeInputs = [jq];
 
   text = ''
-    mkdir -p /tmp/nvidia-patch
-    cp "$1" /tmp/nvidia-patch/patch.sh
-    cd /tmp/nvidia-patch
-    patch -p1 < ${./extract-patch-list.diff} 1>&2
-    bash patch.sh | jq --sort-keys .
+    bash "$1" -j
   '';
 }
